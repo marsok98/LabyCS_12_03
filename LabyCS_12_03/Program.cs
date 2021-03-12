6e43a0d3b80a438c9249ca0743deffcaef6a7865
Mariusz Sokołowski <mariuszeagle98@gmail.com>
2021-03-12 11:51:37 +0100

    Add project files.

---
+++ LabyCS_12_03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace LabyCS_12_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomNumberGenerator z1 = new RandomNumberGenerator(5);
+        }
+    }
+}
